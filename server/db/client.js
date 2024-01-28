4bd34426b19caac2ff8d106f1872d4652bebb95d
ljadow <lindsay@jadow.com>
2024-01-28 16:28:28 -0500

    changed db connection string to Render internal db

--- server/db/client.js
+++ server/db/client.js
@@ -1,6 +1,6 @@
 const { Client } = require('pg');
 const dbName = "taskapp";
 
-const client = new Client(`postgres://localhost:54321/${dbName}`)
+const client = new Client(`postgres://tasklist_e7f4_user:eIebfEjpQHE3ADC4cdeXHudAkXq2ruui@dpg-cmrccsol5elc73aik70g-a/tasklist_e7f4`)
 
 module.exports = client;
